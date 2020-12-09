a4e3fd2d9d131ab155b6a09c394661ab7c973de0
wonghongkeat <hongkeat1991@gmail.com>
2020-12-09 09:23:43 +0000

    edited

---
+++ 01-lab-handson/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <Greeting />
+    <Counter />
+  </div>
+</template>
+
+<script>
+
+import Greeting from './components/sayGreetings';
+import Counter from './components/counter'
+
+export default {
+  name: '#app',
+  components: {
+      Greeting, Counter
+  
+}
+}
+</script>
+
+<style>
+</style>
---
+++ 01-lab-handson/src/components/counter.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <p>{{ number }}</p>
+    <button v-on:click="increase">increase</button>
+    <button v-on:click="decrease">decrease</button>
+     <span v-if="number%2==0">Even</span>
+      <span v-else>Odd</span>
+  </div>
+</template>
+
+
+
+<script>
+export default {
+  data: function () {
+    return {
+      number: 0,
+    };
+  },
+
+  methods: {
+    increase: function () {
+      this.number += 1;
+    },
+    decrease: function () {
+      this.number -= 1;
+    },
+
+
+  },
+};
+</script>
+
+
+
+<style scoped>
+</style>
