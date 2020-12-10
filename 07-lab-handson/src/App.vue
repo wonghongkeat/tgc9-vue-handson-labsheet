d778b908f6a9368c9eff9f0399d8fcfa37f9eea2
wonghongkeat <hongkeat1991@gmail.com>
2020-12-10 09:37:05 +0000

    edited

---
+++ 07-lab-handson/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+<Product/>
+  </div>
+</template>
+
+<script>
+
+import Product from './components/ProductList'
+
+export default {
+  name: 'App',
+  components: {
+      Product
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ 07-lab-handson/src/components/ProductList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+      <h2>Product List</h2>
+      <ul>
+          <li v-for='(i, index) in product' v-bind:key='index'>
+              {{i}}
+            </li>
+        </ul>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+data: function(){
+    return{
+        product: ["Handbags", "Vacuum Cleaner" ]
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
