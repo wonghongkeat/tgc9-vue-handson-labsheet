a774a79d565a9eae36bdc517dfccf6ba0fe341a6
wonghongkeat <hongkeat1991@gmail.com>
2020-12-10 15:18:09 +0000

    edited

--- 07-lab-handson/src/components/ProductList.vue
+++ 07-lab-handson/src/components/ProductList.vue
@@ -5,14 +5,15 @@
       <ul>
           <li v-for='(i, index) in product' v-bind:key='index'>
               {{i}}
-              <button>X</button>
-              <button>edit</button>
+              <button v-on:click='deleteButton(i)'>X</button>
+              <button v-on:click='editButton(index)'>edit</button>
             </li>
         </ul>
         </div>
   <div>
       <label>Enter new product:</label>
       <input type='text' v-model='newProduct'>
+      <button v-on:click='addButton'>Add</button>
   </div>
   </div>
 </template>
@@ -26,6 +27,21 @@ data: function(){
         product: ["Handbags", "Vacuum Cleaner","broom" ],
         newProduct: ''
     }
+    },
+    methods:{
+        addButton: function(){
+            this.product.push(this.newProduct);
+            this.newProduct = ''
+
+        },
+        deleteButton: function(product){
+            let indexDelete = this.product.indexOf(product);
+            this.product.splice(indexDelete,1)
+        },
+        editButton: function(index){
+            let newProductname = prompt('Enter new prodcut')
+            this.$set(this.product, index, newProductname)
+        }
 }
 }
 </script>
